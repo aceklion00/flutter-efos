22ecb983f02d6887a4be6639bda365a5653e0255
Coder Dev <tita.kalpesh@gmail.com>
2021-07-20 21:34:43 +0530

    updated comment

--- lib/models/user_m.dart
+++ lib/models/user_m.dart
@@ -1,6 +1,6 @@
 import 'package:get/get_connect.dart';
 
-//Addning Class Commit
+//Addning
 class UserModel extends GetConnect {
   Future<Response> postUser(Map data) =>
       post('https://api.extrastaff.com/login', data);
