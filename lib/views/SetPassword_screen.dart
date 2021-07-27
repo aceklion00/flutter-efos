70618db568296b7d813b07e48a446616f2407ed4
Coder Dev <tita.kalpesh@gmail.com>
2021-07-27 12:51:29 +0530

    Updated TA-12 and TA-13

---
+++ lib/views/SetPassword_screen.dart
@@ -0,0 +1,278 @@
+import 'package:flutter/material.dart';
+import 'package:adobe_xd/pinned.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class TA12Postregistrationsetcode extends StatelessWidget {
+  TA12Postregistrationsetcode({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff00458d),
+      body: Stack(
+        children: <Widget>[
+          Pinned.fromPins(
+            Pin(start: 0.0, end: -204.0),
+            Pin(size: 124.0, start: 0.0),
+            child:
+                // Adobe XD layer: 'top' (group)
+                Stack(
+              children: <Widget>[
+                Container(),
+                Pinned.fromPins(
+                  Pin(size: 244.0, start: 92.0),
+                  Pin(size: 43.0, end: 0.0),
+                  child: Text(
+                    'Set Password',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 35,
+                      color: const Color(0xffffffff),
+                      letterSpacing: 0.14,
+                      fontWeight: FontWeight.w600,
+                      height: 0.7428571428571429,
+                    ),
+                    textHeightBehavior:
+                        TextHeightBehavior(applyHeightToFirstAscent: false),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(start: 0.0, end: 0.0),
+            Pin(size: 44.0, start: 0.0),
+            child:
+                // Adobe XD layer: 'Device top' (group)
+                Stack(
+              children: <Widget>[
+                Container(),
+              ],
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(size: 236.0, start: 52.0),
+            Pin(size: 29.0, middle: 0.2029),
+            child:
+                // Adobe XD layer: 'Please enter your e…' (text)
+                Text(
+              'Enter Password',
+              style: TextStyle(
+                fontFamily: 'SofiaProRegular',
+                fontSize: 13,
+                color: const Color(0xffffffff),
+                height: 1.3076923076923077,
+              ),
+              textHeightBehavior:
+                  TextHeightBehavior(applyHeightToFirstAscent: false),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(start: 52.0, end: 52.0),
+            Pin(size: 65.0, middle: 0.2555),
+            child:
+                // Adobe XD layer: 'Group 17878' (group)
+                Stack(
+              children: <Widget>[
+                Pinned.fromPins(
+                  Pin(start: 0.0, end: 0.0),
+                  Pin(start: 0.0, end: 0.0),
+                  child:
+                      // Adobe XD layer: 'Rectangle 4143' (shape)
+                      Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14.0),
+                      color: const Color(0xfff7f7f7),
+                      border: Border.all(
+                          width: 1.0, color: const Color(0xffffffff)),
+                    ),
+                  ),
+                ),
+                Pinned.fromPins(
+                  Pin(startFraction: 0.0617, endFraction: 0.7068),
+                  Pin(size: 22.0, middle: 0.4651),
+                  child:
+                      // Adobe XD layer: 'arlenemccoy@gmail.c…' (text)
+                      Text(
+                    'Password',
+                    style: TextStyle(
+                      fontFamily: 'SofiaPro-Regular',
+                      fontSize: 17,
+                      color: const Color(0xffadadb8),
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(size: 248.0, middle: 0.5056),
+            Pin(size: 60.0, middle: 0.5139),
+            child:
+                // Adobe XD layer: 'Group 17532' (group)
+                Stack(
+              children: <Widget>[
+                Pinned.fromPins(
+                  Pin(start: 0.0, end: 0.0),
+                  Pin(start: 0.0, end: 0.0),
+                  child:
+                      // Adobe XD layer: 'Group 17531' (group)
+                      Stack(
+                    children: <Widget>[
+                      Pinned.fromPins(
+                        Pin(start: 0.0, end: 0.0),
+                        Pin(start: 0.0, end: 0.0),
+                        child:
+                            // Adobe XD layer: 'Group 17580' (group)
+                            Stack(
+                          children: <Widget>[
+                            Pinned.fromPins(
+                              Pin(start: 0.0, end: 0.0),
+                              Pin(start: 0.0, end: 0.0),
+                              child:
+                                  // Adobe XD layer: 'Rectangle 139' (shape)
+                                  Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(28.5),
+                                  color: const Color(0xfffe724c),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: const Color(0x33fe724c),
+                                      offset: Offset(0, 10),
+                                      blurRadius: 30,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Pinned.fromPins(
+                              Pin(startFraction: 0.4072, endFraction: 0.4153),
+                              Pin(size: 15.0, middle: 0.5),
+                              child:
+                                  // Adobe XD layer: 'Send new password' (text)
+                                  Text(
+                                'NEXT',
+                                style: TextStyle(
+                                  fontFamily: 'SofiaProMedium',
+                                  fontSize: 15,
+                                  color: const Color(0xffffffff),
+                                  letterSpacing: 1.2,
+                                  height: 1,
+                                ),
+                                textHeightBehavior: TextHeightBehavior(
+                                    applyHeightToFirstAscent: false),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(size: 38.0, start: 17.0),
+            Pin(size: 38.0, start: 53.0),
+            child:
+                // Adobe XD layer: 'Group 17955' (group)
+                Stack(
+              children: <Widget>[
+                Pinned.fromPins(
+                  Pin(start: 0.0, end: 0.0),
+                  Pin(start: 0.0, end: 0.0),
+                  child:
+                      // Adobe XD layer: 'Rectangle 105' (shape)
+                      SvgPicture.string(
+                    _svg_jhcnl3,
+                    allowDrawingOutsideViewBox: true,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Pinned.fromPins(
+                  Pin(size: 5.0, middle: 0.5152),
+                  Pin(size: 9.7, middle: 0.4944),
+                  child:
+                      // Adobe XD layer: 'Path 3391' (shape)
+                      SvgPicture.string(
+                    _svg_u37pb6,
+                    allowDrawingOutsideViewBox: true,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(size: 236.0, start: 52.0),
+            Pin(size: 29.0, middle: 0.3434),
+            child:
+                // Adobe XD layer: 'Please enter your e…' (text)
+                Text(
+              'Re-type Password',
+              style: TextStyle(
+                fontFamily: 'SofiaProRegular',
+                fontSize: 13,
+                color: const Color(0xffffffff),
+                height: 1.3076923076923077,
+              ),
+              textHeightBehavior:
+                  TextHeightBehavior(applyHeightToFirstAscent: false),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(start: 52.0, end: 52.0),
+            Pin(size: 65.0, middle: 0.4019),
+            child:
+                // Adobe XD layer: 'Group 17878' (group)
+                Stack(
+              children: <Widget>[
+                Pinned.fromPins(
+                  Pin(start: 0.0, end: 0.0),
+                  Pin(start: 0.0, end: 0.0),
+                  child:
+                      // Adobe XD layer: 'Rectangle 4143' (shape)
+                      Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14.0),
+                      color: const Color(0xfff7f7f7),
+                      border: Border.all(
+                          width: 1.0, color: const Color(0xffffffff)),
+                    ),
+                  ),
+                ),
+                Pinned.fromPins(
+                  Pin(startFraction: 0.0617, endFraction: 0.7068),
+                  Pin(size: 22.0, middle: 0.4651),
+                  child:
+                      // Adobe XD layer: 'arlenemccoy@gmail.c…' (text)
+                      Text(
+                    'Password',
+                    style: TextStyle(
+                      fontFamily: 'SofiaPro-Regular',
+                      fontSize: 17,
+                      color: const Color(0xffadadb8),
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+const String _svg_jhcnl3 =
+    '<svg viewBox="0.0 0.0 38.0 38.0" ><path  d="M 12 0 L 26 0 C 32.62741851806641 0 38 5.372582912445068 38 12 L 38 26 C 38 32.62741851806641 32.62741851806641 38 26 38 L 12 38 C 5.372582912445068 38 0 32.62741851806641 0 26 L 0 12 C 0 5.372582912445068 5.372582912445068 0 12 0 L 12 0 Z" fill="#393948" stroke="none" stroke-width="1" stroke-miterlimit="10" stroke-linecap="butt" /></svg>';
+const String _svg_u37pb6 =
+    '<svg viewBox="17.0 14.0 5.0 9.7" ><path transform="translate(17.0, 14.0)" d="M 5 0 L 0 4.683928966522217 L 5 9.683929443359375" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /></svg>';
