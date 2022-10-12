7d65e29ce24bcf6c0b178fa5c5694663bc7fbc33
Luka_FairDev <fairdev9610@outlook.com>
2022-10-12 17:18:15 +0100

    overwrited with new codes

--- lib/models/user_m.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get/get_connect.dart';
-
-class UserModel extends GetConnect {
-  Future<Response> postUser(Map data) =>
-      post('https://development.services.extrastaff.com/login', data,
-          headers: {'lang': 'english'});
-}
