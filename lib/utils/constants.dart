8559271cafb352e7f13bb22f630bd5452561407a
Luka_FairDev <luka.mobiledev@xplore4.com>
2022-12-30 15:28:03 +0100

    video updated

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -24,7 +24,7 @@ bool get isDriver => localStorage?.getBool('isDriver') ?? false;
 
 bool is35T = false;
 bool isQuizTest = false;
-String versionStr = "20221230_2";
+String versionStr = "20221230_3";
 // final bool isWebApp = (defaultTargetPlatform != TargetPlatform.android &&
 //     defaultTargetPlatform != TargetPlatform.iOS);
 
