815141aa9f34dfb6b9b863c90eae2e66557a6880
Luka_FairDev <luka.mobiledev@xplore4.com>
2022-12-30 11:00:11 +0100

    version 1.0.2+16 -restored 2FA bypasscode

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -24,7 +24,7 @@ bool get isDriver => localStorage?.getBool('isDriver') ?? false;
 
 bool is35T = false;
 bool isQuizTest = false;
-String versionStr = "20221225";
+String versionStr = "20221230";
 // final bool isWebApp = (defaultTargetPlatform != TargetPlatform.android &&
 //     defaultTargetPlatform != TargetPlatform.iOS);
 
